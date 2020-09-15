e374cb5f9b8994ee9a11745e2e67b63382f1a288
marlon-developer <marlonti17@gmail.com>
2020-09-15 01:08:33 -0300

    AdminLTE3 3.0.5 ReactJS

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/core/Header';
+import Menu from './components/core/Menu';
+import Body from './components/core/Body';
+import Footer from './components/core/Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/core/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/body.css';
+
+function Body() {
+    return (
+        <div className="content-wrapper">
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default Body;
